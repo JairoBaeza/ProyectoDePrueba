c51015ba19d48787d43c43232ebad4cc43ccf731
JairoBaeza <jairo.baezag@correoaiep.cl>
2021-09-20 21:39:51 -0300

    21:39

--- ProyectoDePrueba/Pages/Index.cshtml.cs
+++ ProyectoDePrueba/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ProyectoDePrueba.Repositorios;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,16 @@ namespace ProyectoDePrueba.Pages
 {
     public class IndexModel : PageModel
     {
+        [Display(Name="Usuario")]
+        [BindProperty]
+        [Required(ErrorMessage ="El campo usuario es requerido")]
+        public string NombreUsuario { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [BindProperty]
+        [Required(ErrorMessage = "El campo contraseña es requerido")]
+        public string Password { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +32,29 @@ namespace ProyectoDePrueba.Pages
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = this.NombreUsuario;
+                var password = this.Password;
+                //Validar en la BD
+                var repo = new LoginRepositorio();
+                bool resultado = repo.UsuarioExiste(usuario, password);
+                if (resultado == true)
+                {
+                    return RedirectToPage("./Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty,"El usuario o contraseña no es valido");
+                    return Page();
+                }
+                
+            }
+            return Page();
         }
     }
 }
---
+++ ProyectoDePrueba/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoDePrueba.Repositorios
+{
+    public class LoginRepositorio
+    {
+        public bool UsuarioExiste(string nombreUsuario, string password)
+        {
+            var respuesta = false;
+            var connectionString = "server=localhost;database=mantenedorBD;Integrated Security=true;";
+            using SqlConnection sql = new SqlConnection(connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_check_usuario",sql);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@nombreUsuario", nombreUsuario));
+            cmd.Parameters.Add(new SqlParameter("@password", password));
+            sql.Open();
+            var resultadoQuery = (int) cmd.ExecuteScalar();
+            
+            if (resultadoQuery > 0)
+            {
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+    }
+}
